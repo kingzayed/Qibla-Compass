c7f07560be26b1193b44fa83d05c33e70dd02968
zayedshaikh672@gmail.com <Darkknight@12>
2020-10-03 12:38:13 +0530

    branch_name_1:Convert files from java to kotlin

--- app/src/main/java/com/zayed/admin/compass/MainActivity.kt
+++ app/src/main/java/com/zayed/admin/compass/MainActivity.kt
@@ -1,65 +1,51 @@
-package com.zayed.admin.compass;
+package com.zayed.admin.compass
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import android.widget.TextView
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-
-    private ImageView imageView;
-    private TextView textView;
-    private SensorManager sensorManager;
-    private float currentdegree = 0f;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        imageView = (ImageView)findViewById(R.id.compassimages);
-        textView = (TextView)findViewById(R.id.headingnames);
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private var imageView: ImageView? = null
+    private var textView: TextView? = null
+    private var sensorManager: SensorManager? = null
+    private var currentdegree = 0f
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView = findViewById<View>(R.id.compassimages) as ImageView
+        textView = findViewById<View>(R.id.headingnames) as TextView
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),sensorManager.SENSOR_DELAY_GAME);
-
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this, sensorManager!!.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME)
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-        float degree = Math.round(event.values[0]);
-        textView.setText(Float.toString(degree)+"°");
-
-        RotateAnimation rotateAnimation = new RotateAnimation(currentdegree,-degree, RotateAnimation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
-        rotateAnimation.setDuration(210);
-        rotateAnimation.setFillAfter(true);
-        imageView.startAnimation(rotateAnimation);
-        currentdegree = -degree;
-
-
-
-
+    override fun onSensorChanged(event: SensorEvent) {
+        val degree = Math.round(event.values[0]).toFloat()
+        textView!!.text = java.lang.Float.toString(degree) + "°"
+        val rotateAnimation = RotateAnimation(currentdegree, -degree, RotateAnimation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        rotateAnimation.duration = 210
+        rotateAnimation.fillAfter = true
+        imageView!!.startAnimation(rotateAnimation)
+        currentdegree = -degree
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
         //no use
     }
 }
--- app/src/main/java/com/zayed/admin/compass/SplashScreen.kt
+++ app/src/main/java/com/zayed/admin/compass/SplashScreen.kt
@@ -1,27 +1,22 @@
-package com.zayed.admin.compass;
+package com.zayed.admin.compass
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
 
-public class SplashScreen extends Activity {
-
-    private static int SPLASH_TIME_OUT = 700;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splashscreen);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-
-        },SPLASH_TIME_OUT);
+class SplashScreen : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+        Handler().postDelayed({
+            val intent = Intent(this@SplashScreen, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
     }
 
+    companion object {
+        private const val SPLASH_TIME_OUT = 700
+    }
 }
